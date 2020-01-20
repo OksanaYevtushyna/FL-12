866acf5d6bc17cecad4c6b988a4780f98bf20032
Oksana Yevtushyna <44099876+OksanaYevtushyna@users.noreply.github.com>
2020-01-20 21:06:37 +0200

    remake some func, battle has left to change

--- FL12_HW10/homework/js/fighters_game.js
+++ FL12_HW10/homework/js/fighters_game.js
@@ -2,6 +2,7 @@ class Fighter {
     constructor(proper) {
         proper.losses = 0;
         proper.wins = 0;
+        proper.health = proper.hp;
         return {
             getName() {
                 return proper.name;
@@ -16,50 +17,59 @@ class Fighter {
                 return proper.agility;
             },
             getHealth() {
-                return proper.hp;
+                return proper.health;
             },
             dealDamage(healthPoints) {
-                proper.hp = proper.hp - healthPoints >= 0 ? proper.hp - healthPoints : 0;
+                return proper.health = proper.health - healthPoints >= 0 ? proper.health - healthPoints : 0;
             },
-            heal(healthPoints) { //need to remake this function, works incorrectly
-                let totalHp = proper.getHealth();
-                proper.hp = proper.getHealth() + healthPoints <= totalHp ? proper.getHealth() + healthPoints : totalHp;
+            heal(healthPoints) {
+                return proper.health = proper.health + healthPoints <= proper.hp ? proper.health + healthPoints : proper.hp;
             },
             addWin() {
-                return proper.wins++;
+                proper.wins++;
             },
             addLoss() {
-                return proper.losses++;
+                proper.losses++;
             },
             attack(defender) {
                 let maxPersent = 100;
                 let random = Math.floor(Math.random() * maxPersent);
-                let successProbability = maxPersent - (proper.strength + proper.agility);
+                let successProbability = maxPersent - (defender.getStrength() + defender.getAgility());
                 if (random <= successProbability) {
-                    let defDam = defender.getHealth() - this.getDamage();
-                    console.log(`${this.getName()} makes ${defDam} damage to ${defender.getName()}`);
+                    defender.dealDamage(this.getDamage());
+                    console.log(`${this.getName()} makes ${this.getDamage()} damage to ${defender.getName()}`);
                 } else {
                     console.log(`${defender.getName()} attack missed`);
                 }
             },
-            /*battle(fighter1, fighter2) { another logic need here!!!
-                //code
-                if (fighter1.getHealth() <= 0) {
-                    console.log(`${fighter2.getName()} has won!`);
-                    fighter2.addWin();
-                    fighter1.addLoss();
-                } else if (fighter2.getHealth() <= 0) {
-                    console.log(`${fighter1.getName()} has won!`);
-                    fighter1.addWin();
-                    fighter2.addLoss();
-                }
-            },*/
             logCombatHistory() {
-                console.log(`Name: ${this.getName()}, Wins: ${this.addWin()}, Losses: ${this.addLoss()}`);
+                console.log(`Name: ${this.getName()}, Wins: ${proper.wins}, Losses: ${proper.losses}`);
             }
         }
     }
 }
 
-const fighter1 = new Fighter({ name: 'Maximus', damage: 25, hp: 100, strength: 30, agility: 25 });
-const fighter2 = new Fighter({ name: 'Comos', damage: 25, hp: 90, strength: 20, agility: 35 });
+battle = (...fighters) => {
+    if (fighters[0].getHealth() <= 0 || fighters[1].getHealth() <= 0) {
+        console.log(`${this.getName()} is dead and can't fight!`);
+    } else {
+        fighters[0].attack(fighters[1]);
+        if (fighters[1].getHealth()) {
+            fighters[1].attack(fighters[0]);
+            if (fighters[0].getHealth()) {
+                battle(...fighters);
+            } else {
+                console.log(`${fighters[1].getName()} has won!`);
+                fighters[1].addWin();
+                fighters[0].addLoss();
+            }
+        } else {
+            console.log(`${fighters[0].getName()} has won!`);
+            fighters[0].addWin();
+            fighters[1].addLoss();
+        }
+    }
+}
+
+const fighter1 = new Fighter({ name: 'Maximus', damage: 15, hp: 100, strength: 30, agility: 25 });
+const fighter2 = new Fighter({ name: 'Comos', damage: 22, hp: 90, strength: 20, agility: 35 });
